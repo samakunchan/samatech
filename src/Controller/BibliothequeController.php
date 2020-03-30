faf036de40312e1a470ce18c3b03bccc6bccb91d
Samakunchan <samakunchan@gmail.com>
2020-03-30 07:47:50 +0200

    feat(01-document): document basique
    - Ajout et config du fichier FileService.php pour l'ajout simple de fichier
    - Création d'une entité Document.php
    - Création du BibliothequeController.php pour gérer les documents
    - Création d'un DocumentType.php pour afficher le formalaire d'ajout simple
    - Listes des documents afficher dans un tableau
    - Ajout d'un svg pdf

---
+++ src/Controller/BibliothequeController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Document;
+use App\Form\DocumentType;
+use App\Repository\DocumentRepository;
+use App\Service\FileService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/bibliotheque")
+ */
+class BibliothequeController extends AbstractController
+{
+    /**
+     * @Route("/", name="bibliotheque")
+     * @param DocumentRepository $documentRepository
+     * @return Response
+     */
+    public function index(DocumentRepository $documentRepository)
+    {
+        dump($documentRepository->findAll());
+        return $this->render('bibliotheque/index.html.twig', [
+            'documents' => $documentRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/add", name="bibliotheque_add_documents")
+     * @param Request $request
+     * @param FileService $service
+     * @param EntityManagerInterface $entityManager
+     * @return Response
+     */
+    public function add(Request $request, FileService $service, EntityManagerInterface $entityManager): Response
+    {
+        $document = new Document();
+        $form = $this->createForm(DocumentType::class, $document);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $type = 'image';
+            if ($form->getData()->getFile()->getMimeType() === 'application/pdf') {
+                $type = 'pdf';
+            }
+            $service->upload($form->getData()->getFile(), $document, $type);
+
+            $entityManager->persist($document);
+            $entityManager->flush();
+        }
+        return $this->render('bibliotheque/add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
