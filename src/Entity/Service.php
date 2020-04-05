658bdb614ccd665e1d5c99f360a8eb9017628b77
Samakunchan <samakunchan@gmail.com>
2020-04-05 19:18:03 +0200

    feat(entity): service
    - Création de l'entité Service.php
    - Création de repository ServiceRepository.php
    - Configuration de l'assertion dans l'entité

---
+++ src/Entity/Service.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ServiceRepository")
+ */
+class Service
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     */
+    private $description;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Document", cascade={"persist", "remove"})
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     */
+    private $icone;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Document", cascade={"persist", "remove"})
+     * @Assert\NotBlank(message="Le champ ne doit pas être vide")
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getIcone(): ?Document
+    {
+        return $this->icone;
+    }
+
+    public function setIcone(?Document $icone): self
+    {
+        $this->icone = $icone;
+
+        return $this;
+    }
+
+    public function getImage(): ?Document
+    {
+        return $this->image;
+    }
+
+    public function setImage(?Document $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
