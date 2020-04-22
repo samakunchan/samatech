cc6707c43bcaf6ce2a6967cd0a09015bc64757f8
Samakunchan <samakunchan@gmail.com>
2020-04-22 22:55:40 +0200

    feat(crud): blog
    - Ajout du BlogController.php
    - Ajout des templates du BlogController.php dont un template réservé à la sidebar
    - Ajout du BlogType.php pour le crud
    - Création d'une méthode pour récupérer toute les catégories de l'environnment Blog dans le CategoryRepository.php
    - Création d'une méthode pour récupérer toute les tags de l'environnment Blog dans le TagRepository.php
    - Ajout du CDN Owl Caroussel JS et CSS pour slider les catégories dans le front
    - Intégration du service Disqus pour les commentaires

---
+++ src/Form/BlogType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Blog;
+use App\Entity\Category;
+use App\Form\Type\TagsInputType;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BlogType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class, ['required' => false])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er
+                        ->createQueryBuilder('c')
+                        ->andWhere('c.environnement = :val')
+                        ->setParameter('val', '2')
+                        ;
+                },
+                'choice_label' => 'type'
+            ])
+            ->add('mainImage', CollectionType::class, [
+                'entry_type'   => DocumentType::class,
+                'allow_add'    => true,
+                'allow_delete' => true,
+                'required' => false,
+            ])
+            ->add('tags', TagsInputType::class, [
+                'label' => 'Ajouter des tags',
+                'required' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Blog::class,
+        ]);
+    }
+}
--- src/Repository/TagRepository.php
+++ src/Repository/TagRepository.php
@@ -19,6 +19,15 @@ class TagRepository extends ServiceEntityRepository
         parent::__construct($registry, Tag::class);
     }
 
+    public function findAllBlogsTags()
+    {
+        return $this->createQueryBuilder('t')
+            ->innerJoin('t.blogs', 'b')
+            ->getQuery()
+            ->getResult()
+            ;
+    }
+
     // /**
     //  * @return Tag[] Returns an array of Tag objects
     //  */
